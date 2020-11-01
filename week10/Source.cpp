d9130547e4d2092ca10b2529332645d376ceb40e
PIYAWAN NORPALM <63010608@KMITL.AC.TH>
2020-11-01 21:53:43 +0700

    Add project files.

---
+++ week10/Source.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int matrix[3][3], r, c;
+	for (r = 0; r < 3; r++)
+	{
+		for (c = 0; c < 3; c++)
+		{
+			printf("Enter numbers [%d][%d] : ", r, c);
+			scanf_s("%d", &matrix[r][c]);
+		}
+	}
+		printf("\n*** Matrix ***\n");
+	for (r = 0; r < 3; r++)
+	{
+		for (c = 0; c < 3; c++)
+		{
+			printf("%5d ", matrix[r][c]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
